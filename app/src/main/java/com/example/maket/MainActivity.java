275868c65867ec769781a306660e280082ed474f
лебедевааф <sashyla1809@gmail.com>
2022-11-16 18:19:14 +0300

    все страницы

--- app/src/main/java/com/example/maket/MainActivity.java
+++ app/src/main/java/com/example/maket/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.maket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final int SPLASH_DISPLAY_LENGHT = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//заставка
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          Intent mainIntent = new Intent(MainActivity.this, LogIn.class);
+                                          MainActivity.this.startActivity(mainIntent);
+                                          MainActivity.this.finish();
+                                      }
+                                  }, SPLASH_DISPLAY_LENGHT);
     }
 }
