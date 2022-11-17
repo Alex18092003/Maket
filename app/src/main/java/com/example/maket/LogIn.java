27c0f581b0ad7f16a67869ada11138744d94a304
AleksandraLebedeva <sashyla1809@gmail.com>
2022-11-18 00:17:41 +0300

    регистрация

--- app/src/main/java/com/example/maket/LogIn.java
+++ app/src/main/java/com/example/maket/LogIn.java
@@ -3,12 +3,29 @@ package com.example.maket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LogIn extends AppCompatActivity {
-
+    ImageView image, imageCheked;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        imageCheked = findViewById(R.id.cbRememberPassword);
+
+    }
+    public void Checked(View v)
+    {
+        if(imageCheked.getDrawable().getConstantState() == getResources().getDrawable( R.drawable.check_box_not_active).getConstantState())
+        {
+            imageCheked.setImageResource(R.drawable.check_box_active);
+        }
+        else
+        {
+            imageCheked.setImageResource(R.drawable.check_box_not_active);
+        }
+
     }
 }
